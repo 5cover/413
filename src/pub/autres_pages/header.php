e7b332fb4e5e997b1c62503f0fa9cc73d621caff
5cover <thediscover22450@gmail.com>
2024-10-16 16:57:04 +0200

    support

--- src/pub/autres_pages/header.php
+++
@@ -1,9 +0,0 @@
-<header>
-    <div class="logo">
-        <img src="../images/logo.png" alt="Logo pact">
-    </div>
-    <div class="auth-button">
-        <img src="../images/profile-icon.png" alt="Profil">
-        <span>Déconnexion</span>
-    </div>
-</header>
