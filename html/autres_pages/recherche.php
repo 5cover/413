6dcbbc0dd5d223db5e1fa1665996664d017a4a4c
marius <icelos@yahoo.com>
2025-03-10 13:57:13 +0100

    tst map

--- html/autres_pages/recherche.php
+++ html/autres_pages/recherche.php
@@ -68,6 +68,11 @@ $page->put(function () {
     <?php 
     
     ?>
+    <script>var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);</script>
 
     <template id="template-offer-card"><?php CarteOffre::put_template() ?></template>
     <?php
