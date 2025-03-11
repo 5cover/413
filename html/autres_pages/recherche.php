21f077a4bf682be2fe7508f5827679ca305850d3
Benjamin <benjamin.dumontgirard@gmail.com>
2025-03-11 13:54:57 +0100

    OK RECHERCHE REMARCHE

--- html/autres_pages/recherche.php
+++ html/autres_pages/recherche.php
@@ -37,47 +37,49 @@ $page->put(function () {
         </div>
     </section>
 
-    <section class="tag-selection">
-        <div class="categories">
-            <h3>Catégories</h3>
-            <div class="category-dropdown">
-                <select id="main-category" onchange="showSubcategories()">
-                    <option value="">-- Toutes les catégories --</option>
-                    <option value="restaurant">Restauration</option>
-                    <option value="activité">Activité</option>
-                    <option value="visite">Visite</option>
-                    <option value="spectacle">Spectacle</option>
-                    <option value="parc_d_attractions">Parc d'attractions</option>
-                </select>
+    <div class="criteres">
+        <section class="tag-selection">
+            <div class="categories">
+                <h3>Catégories</h3>
+                <div class="category-dropdown">
+                    <select id="main-category" onchange="showSubcategories()">
+                        <option value="">-- Toutes les catégories --</option>
+                        <option value="restaurant">Restauration</option>
+                        <option value="activité">Activité</option>
+                        <option value="visite">Visite</option>
+                        <option value="spectacle">Spectacle</option>
+                        <option value="parc_d_attractions">Parc d'attractions</option>
+                    </select>
+                </div>
             </div>
-        </div>
-        <input type="hidden" id="selected-category" name="category" value="">
-        <div id="subcategories" class="hidden">
-            <h3>Tags</h3>
-            <div class="subcategory-list" id="subcategory-list">
-
+            <input type="hidden" id="selected-category" name="category" value="">
+            <div id="subcategories" class="hidden">
+                <h3>Tags</h3>
+                <div class="subcategory-list" id="subcategory-list">
+                    
+                </div>
             </div>
-        </div>
-    </section>
-    <section class="sorting-section">
-        <br>
-        <h3>Options de tri</h3>
-        <div class="sorting-buttons">
-            <button id="sort-price-down" class="btn-sort" data-criteria="prix">Prix croissant</button>
-            <button id="sort-price-up" class="btn-sort" data-criteria="prix">Prix décroissant</button>
-            <button id="sort-rating-down" class="btn-sort" data-criteria="note">Note croissante</button>
-            <button id="sort-rating-up" class="btn-sort" data-criteria="note">Note décroissante</button>
-            <button id="sort-date-up" class="btn-sort" data-criteria="date">Plus récent</button>
-            <button id="sort-date-down" class="btn-sort" data-criteria="date">Moins récent</button>
-        </div>
-    </section>
-    <section class="highlight-offers">
-        <h2>Offres trouvées :</h2>
-        <div class="offer-list">
-
-        </div>
-    </section>
-    
+        </section>
+        <section class="sorting-section">
+            <br>
+            <h3>Options de tri</h3>
+            <div class="sorting-buttons">
+                <button id="sort-price-down" class="btn-sort" data-criteria="prix">Prix croissant</button>
+                <button id="sort-price-up" class="btn-sort" data-criteria="prix">Prix décroissant</button>
+                <button id="sort-rating-down" class="btn-sort" data-criteria="note">Note croissante</button>
+                <button id="sort-rating-up" class="btn-sort" data-criteria="note">Note décroissante</button>
+                <button id="sort-date-up" class="btn-sort" data-criteria="date">Plus récent</button>
+                <button id="sort-date-down" class="btn-sort" data-criteria="date">Moins récent</button>
+            </div>
+        </section>
+        <section class="highlight-offers">
+            <h2>Offres trouvées :</h2>
+            <div class="offer-list">
+                
+            </div>
+        </section>
+    </div>
+    <!-- ici -->
     <template id="template-offer-card"><?php CarteOffre::put_template() ?></template>
     
     <section class="map-section">
