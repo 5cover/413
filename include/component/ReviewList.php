20435cc9a307919bca1ab1977f8739e4c6923905
Scover <bardini.raphael@gmail.com>
2025-03-11 10:45:07 +0100

    l

--- include/component/ReviewList.php
+++ include/component/ReviewList.php
@@ -22,10 +22,12 @@ final class ReviewList
                 <p>Moyenne&nbsp;: <?= $this->offre->note_moyenne ?? 0 ?>/5 ★</p>
                 <div class="rating-distribution">
                     <?php
-                    $avis               = iterator_to_array(
-                        Avis::from_db_all(id_offre: $this->offre->id,
-                            blackliste: $this->est_connecte_pro_proprio() && $this->offre->libelle = 'premium' ? null : false)
-                    );
+                    $avis               = iterator_to_array(Avis::from_db_all(
+                        id_offre: $this->offre->id,
+                        blackliste: $this->est_connecte_pro_proprio() && $this->offre->libelle = 'premium'
+                            ? null
+                            : false
+                    ));
                     $avis_count_by_note = [1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0];
                     foreach ($avis as $a) {
                         ++$avis_count_by_note[$a->note];
