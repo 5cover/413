4a371e59412e72b4f390eee21d09399061dd74a1
marius <icelos@yahoo.com>
2025-03-12 12:12:07 +0100

    changement lin icon

--- html/script_js/tri_recherche.js
+++ html/script_js/tri_recherche.js
@@ -222,12 +222,12 @@ function updateMapWithIcons(offersToDisplay) {
 
     // Définition des icônes personnalisées selon la catégorie principale
     const icons = {
-        restaurant: L.icon({ iconUrl: "/html/images/icons/restaurant.png", iconSize: [40, 40] }),
-        activité: L.icon({ iconUrl: "/html/images/icons/activite.png", iconSize: [40, 40] }),
-        visite: L.icon({ iconUrl: "/html/images/icons/visite.png", iconSize: [40, 40] }),
-        spectacle: L.icon({ iconUrl: "/html/images/icons/spectacle.png", iconSize: [40, 40] }),
-        parc_d_attraction: L.icon({ iconUrl: "/html/images/icons/parc.png", iconSize: [40, 40] }),
-        default: L.icon({ iconUrl: "/html/images/icons/default.png", iconSize: [40, 40] }) // Icône par défaut
+        restaurant: L.icon({ iconUrl: "../images/icons/restaurant.png", iconSize: [40, 40] }),
+        activité: L.icon({ iconUrl: "../images/icons/activite.png", iconSize: [40, 40] }),
+        visite: L.icon({ iconUrl: "../images/icons/visite.png", iconSize: [40, 40] }),
+        spectacle: L.icon({ iconUrl: "../images/icons/spectacle.png", iconSize: [40, 40] }),
+        parc_d_attraction: L.icon({ iconUrl: "../images/icons/parc-attraction.png", iconSize: [40, 40] }),
+        default: L.icon({ iconUrl: "..images/icons/place-marker.png", iconSize: [40, 40] }) // Icône par défaut
     };
 
     offersToDisplay.forEach(offer => {
