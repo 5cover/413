588bbf31482d65daa448237f34642c2e8a74e465
Scover <bardini.raphael@gmail.com>
2025-03-12 12:23:28 +0100

    fix

--- html/json/offres.php
+++ html/json/offres.php
@@ -16,8 +16,9 @@ foreach ($offres as &$o) {
     $o['prix_min']           = floatval($o['prix_min']);
     $o['formatted_address']  = Adresse::from_db($o['id_adresse'])->format();
     $o['tags']               = json_decode($o['tags']);
-    $o['option']             = json_decode($o['option']);
     $o['periodes_ouverture'] = MultiRange::parse($o['periodes_ouverture'], FiniteTimestamp::parse(...))->ranges;
+    
+    if (isset($o['option'])) $o['option'] = json_decode($o['option']);
 }
 
 echo notfalse(json_encode($offres));
