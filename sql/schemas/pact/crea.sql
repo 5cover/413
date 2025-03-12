f8e655996f46cf3e06d689ee96bff69f7eabd12f
Soladoc <romaingrandchamppro@gmail.com>
2025-03-12 10:44:44 +0100

    répare temps

--- sql/schemas/pact/crea.sql
+++ sql/schemas/pact/crea.sql
@@ -244,7 +244,7 @@ create table _blacklist (
     id int
         constraint blacklist_pk primary key
         constraint blacklist_inherits_avis references _avis on delete cascade,
-    date_blacklist timestamp default now,
+    date_blacklist timestamp default localtimestamp,
     fin_blacklist timestamp 
 );
 
