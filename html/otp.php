efb2ec031fd0bab6833e9dfd8f1c1019e75857c6
Scover <bardini.raphael@gmail.com>
2025-03-18 17:04:12 +0100

    otp begin qr code

---
+++ html/otp.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'auth.php';
+require_once 'otp.php';
+
+$id_compte = Auth\exiger_connecte();
+
+$otp_url = OTP\generate_secret($id_compte);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>OTP Setup</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
+</head>
+<body>
+    <h2>Scan this QR Code</h2>
+    <div id="qrcode"></div>
+    <script>new QRCode(document.getElementById("qrcode"), '<?= $otp_url ?>')</script>
+</body>
+</html>
