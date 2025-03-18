342e20d1b62fa7b4adcfcfd2262828d2697dfc6d
Xx-Nasank-xX <maelanpotier05@gmail.com>
2025-03-18 17:39:22 +0100

    error notif bdd arrive

--- sql/schemas/pact/crea.sql
+++ sql/schemas/pact/crea.sql
@@ -334,15 +334,3 @@ create table _souscription_option (
     actif bool not null default true
 );
 
-/*CREATE TABLE _notification (
-    id SERIAL PRIMARY KEY,
-    id_pro INT NOT NULL
-        CONSTRAINT notification_fk_pro REFERENCES _professionnel(id) ON DELETE CASCADE,
-    id_offre INT NOT NULL
-        CONSTRAINT notification_fk_offre REFERENCES _avis(id) ON DELETE CASCADE,
-    message TEXT NOT NULL
-        CONSTRAINT notification_fk_commentaire REFERENCES _avis(commentaire) ON DELETE CASCADE,
-    date TIMESTAMP NOT NULL DEFAULT localtimestamp,
-        CONSTRAINT notification_fk_publie REFERENCES _avis(publie_le) ON DELETE CASCADE,
-    lu BOOLEAN NOT NULL DEFAULT FALSE
-);*/
