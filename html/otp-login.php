5bb477348b305161244ac68a0960795d7f9fee3c
Scover <bardini.raphael@gmail.com>
2025-03-24 16:16:47 +0100

    debug log id compte

--- html/otp-login.php
+++ html/otp-login.php
@@ -1,3 +1,4 @@
+<?php require_once 'auth.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +6,7 @@
 </head>
 <body>
     <h2>Enter OTP</h2>
+    <p>ID compte: <?= Auth\exiger_connecte() ?> </p>
     <form id="otpForm">
         <input type="text" id="otp" placeholder="Enter OTP" required>
         <button type="submit">Verify</button>
--- html/otp-qr.php
+++ html/otp-qr.php
@@ -14,6 +14,7 @@ $otp_url = OTP\generate_secret($id_compte);
     <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
 </head>
 <body>
+    <p>ID compte: <?= $id_compte ?></p>
     <h2>Scan this QR Code</h2>
     <div id="qrcode"></div>
     <script>new QRCode(document.getElementById("qrcode"), '<?= $otp_url ?>')</script>
