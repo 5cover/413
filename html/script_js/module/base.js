1ccf648b775bb46a53b964b6d659e63eac616025
Scover <bardini.raphael@gmail.com>
2025-03-26 14:50:01 +0100

    js notif

--- html/script_js/module/base.js
+++ html/script_js/module/base.js
@@ -7,48 +7,6 @@ for (const e of document.getElementsByClassName('button-signaler')) setup_button
 for (const e of document.getElementsByClassName('button-blacklist')) setup_button_blacklist(e);
 for (const e of document.getElementsByClassName('liker')) setup_liker(e);
 
-{
-    const notif = document.getElementById('btn-notifications');
-    notif?.addEventListener('click', toggleMenu);
-}
-
-function toggleMenu() {
-    var menu = document.getElementById("notif-list");
-    if (menu.style.display === "none" || menu.style.display === "") {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
-}
-
-function fetchNotifications() {
-    fetch('/json/fetch_notifications.php', { 'credentials': 'include', }) 
-        .then(response => response.json())
-        .then(data => {
-            const notifCount = document.getElementById("notif-count");
-            const notifList = document.getElementById("notif-items");
-
-            notifCount.textContent = data.nb_avis_non_lus; 
-
-            notifList.innerHTML = ""; 
-            data.avis.forEach(notif => {
-                const li = document.createElement("li");
-                li.innerHTML = `
-                    <a href="detail_offre_pro.php?id=${notif.auteur}#1">
-                        <strong>${notif.titre_offre}</strong> : 
-                        ${notif.commentaire.length > 25 ? notif.commentaire.substring(0, 25) + "…" : notif.commentaire}
-                    </a>
-                `;
-                notifList.appendChild(li);
-            });
-        })
-        .catch(error => console.error("Erreur lors de la récupération des notifications:", error));
-}
-
-setInterval(fetchNotifications, 10000);
-fetchNotifications();
-
-
 /**
  * @param {HTMLElement} element
  */
