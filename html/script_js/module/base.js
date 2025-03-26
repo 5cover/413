58efd9644e56c462c8b49c33e88cc01edd6250a2
Xx-Nasank-xX <maelanpotier05@gmail.com>
2025-03-26 13:49:44 +0100

    corrige error

--- html/script_js/module/base.js
+++ html/script_js/module/base.js
@@ -20,7 +20,7 @@ function toggleMenu() {
 }
 
 function fetchNotifications() {
-    fetch('fetch_notifications.php') 
+    fetch('html/auto/fetch_notifications.php') 
         .then(response => response.json())
         .then(data => {
             const notifCount = document.getElementById("notif-count");
