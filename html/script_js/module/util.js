1895b1781efc0b6afdf15074672358fafe92769b
Soladoc <romaingrandchamppro@gmail.com>
2025-03-26 09:25:10 +0100

    blacklist

--- html/script_js/module/util.js
+++ html/script_js/module/util.js
@@ -43,6 +43,15 @@ export function location_signaler(id_compte, id_signalable, raison) {
 }
 
 /**
+ * @param {number} id
+ * @param {date} fin_blacklist
+ * @returns {string}
+ */
+export function location_blacklist(id, fin_blacklist) {
+    return '/do/blacklister.php?' + new URLSearchParams({ id, fin_blacklist, });
+}
+
+/**
  * @param {number} id_offre 
  * @returns {string}
  */
