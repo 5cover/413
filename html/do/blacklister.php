126ed4b4608cc5f5e4cccdca0c4e20098e23c4a3
Scover <bardini.raphael@gmail.com>
2025-03-28 14:00:39 +0100

    finite timestamp parse

--- html/do/blacklister.php
+++ html/do/blacklister.php
@@ -1,9 +1,10 @@
 <?php
 require_once 'util.php';
+require_once 'model/FiniteTimestamp.php';
 require_once 'model/Blacklist.php';
 
 $id            = getarg($_GET, 'id', arg_int());
-$date_fin      = getarg($_GET, 'finblacklist', arg_int());
+$date_fin      = FiniteTimestamp::parse(getarg($_GET, 'finblacklist'));
 
 http_response_code(
     Blacklist::toggle_blacklist($id, $date_fin)
