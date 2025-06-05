05cb822626709c198013f9f3e7efe1947b2eb1a0
Scover <bardini.raphael@gmail.com>
2025-06-05 21:03:46 +0200

    faut changer ça aussi

--- scripts/server-cp.bash
+++ scripts/server-cp.bash
@@ -10,6 +10,6 @@ echo "$ARTIFACT_SSH_KEY" >~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh-keyscan -p 22 "$ARTIFACT_HOST" >>~/.ssh/known_hosts
 
-readonly host="debian@$ARTIFACT_HOST"
+readonly host="root@$ARTIFACT_HOST"
 
 scp "$1" "$host:$2"
